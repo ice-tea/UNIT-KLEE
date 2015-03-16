52eb6ec9a6f3c15d30983e860cd83af0372e7919
ice-tea <tmacback@gmail.com>
2015-03-16 14:52:20 +0800

    klee.h

--- include/klee/klee.h
+++ include/klee/klee.h
@@ -34,6 +34,13 @@ extern "C" {
   /// output files, etc.
   void klee_make_symbolic(void *addr, size_t nbytes, const char *name);
 
+  /// klee_make_unit - Make the contents of the object pointer to by \arg
+    ///  unit information.
+    ///
+    /// \arg name - var name.
+    /// \arg nbytes - unit name.
+    void klee_make_unit(const char *name, const char *unit);
+
   /// klee_range - Construct a symbolic value in the signed interval
   /// [begin,end).
   ///
